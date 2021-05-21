57a47d20c74b20ebe33c298426f4bf2ee0bb38c9
seonghyeon <shp35079@naver.com>
2021-05-21 16:15:13 +0900

    [FIX] getImagePromise
    Error fix

--- 2021y/5m/17d - 19d/getImagePromise.js
+++ 2021y/5m/17d - 19d/getImagePromise.js
@@ -3,28 +3,47 @@ const catButton = document.querySelector('#cat');
 const dogUrl = 'https://dog.ceo/api/breeds/image/random';
 const catUrl = 'https://api.thecatapi.com/v1/images/search';
 
-const getDogImages = new Promise((resolve, reject) => {
-    const res = axios.get(dogUrl);
-    if (res) {
-        resolve(function () {
-            const imgURL = res.data.message;
-            const dogImage = document.createElement('img');
-            dogImage.src = imgURL;
-            dogImage.style.height = '300px';
-            document.querySelector('.dogs').appendChild(dogImage);
-        });
-    } else {
-        reject(new Error('There was no response'));
-    }
-});
+const getAnimalImages = (url) =>
+    new Promise((resolve, reject) => {
+        const response = axios.get(url);
+        if (response) {
+            resolve(response);
+        } else {
+            reject();
+        }
+    });
 
-dogButton.addEventListener(
-    'click',
-    getDogImages
-        .then((success) => {
-            success();
+const setImageInfo = ({ image, imgUrl, imgStyle }) => {
+    image.src = imgUrl;
+    Object.keys(imgStyle).forEach((style) => (image.style[style] = imgStyle[style]));
+};
+
+const animalButtonClick = (url) => {
+    getAnimalImages(url)
+        .then((response) => {
+            let imgUrl;
+            if (url.endsWith('random')) {
+                imgUrl = response.data.message;
+            } else {
+                imgUrl = response.data[0].url;
+            }
+            const animalImage = document.createElement('img');
+            const imageInfo = {
+                image: animalImage,
+                imgUrl: imgUrl,
+                imgStyle: { height: '300px' },
+            };
+            setImageInfo(imageInfo);
+            if (url.endsWith('random')) {
+                document.querySelector('.dogs').appendChild(animalImage);
+            } else {
+                document.querySelector('.cats').appendChild(animalImage);
+            }
         })
-        .catch((err) => {
-            console.log(err);
-        })
-);
+        .catch(() => {
+            new Error('There was no response');
+        });
+};
+
+dogButton.addEventListener('click', () => animalButtonClick(dogUrl));
+catButton.addEventListener('click', () => animalButtonClick(catUrl));
