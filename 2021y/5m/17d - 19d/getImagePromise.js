e360383b61d9d6699645a075ee04933eefdf7b12
seonghyeon <shp35079@naver.com>
2021-05-21 19:56:22 +0900

    [UPDATE] getImagePromise
    .

--- 2021y/5m/17d - 19d/getImagePromise.js
+++ 2021y/5m/17d - 19d/getImagePromise.js
@@ -1,9 +1,31 @@
+const dog = Symbol('dog');
+const cat = Symbol('cat');
 const dogButton = document.querySelector('#dog');
 const catButton = document.querySelector('#cat');
 const dogUrl = 'https://dog.ceo/api/breeds/image/random';
 const catUrl = 'https://api.thecatapi.com/v1/images/search';
+const dogParent = document.querySelector('.dogs');
+const catParent = document.querySelector('.cats');
 
-const getAnimalImages = (url) =>
+const dogInfo = {
+    url: dogUrl,
+    parent: dogParent,
+    getImageUrlPath: function (response) {
+        return response.data.message;
+    },
+};
+
+const catInfo = {
+    url: catUrl,
+    parent: catParent,
+    getImageUrlPath: function (response) {
+        return response.data[0].url;
+    },
+};
+
+const infoMap = { [dog]: dogInfo, [cat]: catInfo };
+
+const getRandomImages = (url) =>
     new Promise((resolve, reject) => {
         const response = axios.get(url);
         if (response) {
@@ -18,32 +40,24 @@ const setImageInfo = ({ image, imgUrl, imgStyle }) => {
     Object.keys(imgStyle).forEach((style) => (image.style[style] = imgStyle[style]));
 };
 
-const animalButtonClick = (url) => {
-    getAnimalImages(url)
+const imageButtonClick = (target) => {
+    const url = infoMap[target].url;
+    getRandomImages(url)
         .then((response) => {
-            let imgUrl;
-            if (url.endsWith('random')) {
-                imgUrl = response.data.message;
-            } else {
-                imgUrl = response.data[0].url;
-            }
-            const animalImage = document.createElement('img');
+            const imgUrl = infoMap[target].getImageUrlPath(response);
+            const randomImage = document.createElement('img');
             const imageInfo = {
-                image: animalImage,
+                image: randomImage,
                 imgUrl: imgUrl,
                 imgStyle: { height: '300px' },
             };
             setImageInfo(imageInfo);
-            if (url.endsWith('random')) {
-                document.querySelector('.dogs').appendChild(animalImage);
-            } else {
-                document.querySelector('.cats').appendChild(animalImage);
-            }
+            infoMap[target].parent.appendChild(randomImage);
         })
         .catch(() => {
             new Error('There was no response');
         });
 };
 
-dogButton.addEventListener('click', () => animalButtonClick(dogUrl));
-catButton.addEventListener('click', () => animalButtonClick(catUrl));
+dogButton.addEventListener('click', () => imageButtonClick(dog));
+catButton.addEventListener('click', () => imageButtonClick(cat));
