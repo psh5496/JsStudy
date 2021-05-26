346517d1b999060226858da45eb80b08867bcf8e
seonghyeon <shp35079@naver.com>
2021-05-26 23:13:43 +0900

    [MODIFY] pokemon structure update
    .

--- 2021y/5m/26d/pokemon/pokemon.js
+++ 2021y/5m/26d/pokemon/pokemon.js
@@ -2,30 +2,40 @@ const submitButton = document.querySelector(".submit-button");
 const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/";
 const imgBox = document.querySelector('.img-box');
 const textBox = document.querySelector('.text-box');
-const article = document.getElementsByTagName('article');
+const attribute = document.getElementsByClassName('attribute');
+const submitInput = document.querySelector(".submit-input")
+const ID_FIRST_INDEX = 1;
+const ID_LAST_INDEX = 898;
 
 const getPokemonImage = function(res) {
-    const images = res.data.sprites;
-    const imgArr = [images.front_default, images.back_default, images.front_shiny, images.back_shiny];
+    const {front_default, back_default, front_shiny, back_shiny} = res.data.sprites;
+    const imgArr = [front_default, back_default, front_shiny, back_shiny];
     for(let i = 0; i < 4; i++) {
-        var img = document.createElement('img');
+        const img = document.createElement('img');
         img.src = imgArr[i];
         imgBox.appendChild(img);
     }
 }
 
 const getPokemonText = function(res) {
-    const texts = res.data;
-    const textArr = [texts.name, texts.height, texts.weight];
+    const {name, height, weight} = res.data;
+    const textArr = [name, height, weight];
     for(let i = 0; i < 3; i++) {
-        var span = document.createElement('span');
+        const span = document.createElement('span');
         span.innerText = textArr[i];
         textBox.children[i].appendChild(span);
     }
 }
 
-const getPokemonInfo = async function(number) {
-    const response = await axios.get(pokemonUrl + `${number}`);
+const checkChildElement = function(value) {
+    if(imgBox.childElementCount != 0) {
+        removePokemonInfo();
+    }
+    getPokemonInfo(value);
+}
+
+const getPokemonInfo = async function(id) {
+    const response = await axios.get(pokemonUrl + id);
     getPokemonImage(response);
     getPokemonText(response);
 }
@@ -35,20 +45,17 @@ const removePokemonInfo = function() {
         imgBox.removeChild(imgBox.firstChild);
     }
     for(let i = 0; i < 3; i++) {
-        article[i].removeChild(article[i].lastChild);
+        attribute[i].removeChild(attribute[i].lastChild);
     }
 }
 
 const numberSubmit = function() {
-    let value = document.querySelector(".submit-input").value;
+    const value = submitInput.value;
     document.querySelector(".submit-input").value = '';
-    if(value >= 1 && value <= 898) {
-        if(imgBox.childElementCount != 0) {
-            removePokemonInfo();
-        }
-        getPokemonInfo(value);
+    if(value >= ID_FIRST_INDEX && value <= ID_LAST_INDEX) {
+        checkChildElement(value);
     } else {
-        alert('1부터 898까지의 수를 입력해주세요 !');
+        alert(`${ID_FIRST_INDEX}부터 ${ID_LAST_INDEX}까지의 수를 입력해주세요 !`);
     }
 }
 
